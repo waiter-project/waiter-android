0fb56fc7c906a3196847a3352eebb69076b66703
solal <solalhagege@gmail.com>
2016-02-24 14:36:14 +0100

    Fix layout problem in FullActivity

--- app/src/main/java/com/waiter/waiterpoc/WaiterFragment.java
+++ app/src/main/java/com/waiter/waiterpoc/WaiterFragment.java
@@ -59,7 +59,7 @@ public class WaiterFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_event_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_waiter_list, container, false);
 
         // Set the adapter
         if (view instanceof RecyclerView) {
