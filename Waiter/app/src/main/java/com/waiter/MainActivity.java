db465d8ce915626244af0a0a046396a5568ffb41
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-04-22 02:25:33 -0700

    add entries in nav drawer, icons and footer

--- Waiter/app/src/main/java/com/waiter/MainActivity.java
+++ Waiter/app/src/main/java/com/waiter/MainActivity.java
@@ -94,17 +94,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_events) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_payment) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_history) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
