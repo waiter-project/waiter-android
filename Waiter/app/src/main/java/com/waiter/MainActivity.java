5b913dd740efb79382ea1a4f93c18226f7f034e1
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-04-22 23:40:37 -0700

    add ‘about us’ entry in options menu

--- Waiter/app/src/main/java/com/waiter/MainActivity.java
+++ Waiter/app/src/main/java/com/waiter/MainActivity.java
@@ -85,6 +85,8 @@ public class MainActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.action_about) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
