32d39d5f58a6c9b701b7cbf7c5f35569ab4338ac
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-04-22 23:13:01 -0700

    add zoom controls and location button with permission

--- Waiter/app/src/main/java/com/waiter/MapsFragment.java
+++ Waiter/app/src/main/java/com/waiter/MapsFragment.java
@@ -1,12 +1,17 @@
 package com.waiter;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,10 +22,13 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
 
-    MapView mMapView;
-    private GoogleMap googleMap;
+    private static final int PERMISSIONS_REQUEST_LOCATION = 1;
+    private boolean mPermissionDenied = false;
+
+    private MapView mMapView;
+    private GoogleMap mGoogleMap;
 
     private OnFragmentInteractionListener mListener;
 
@@ -36,7 +44,8 @@ public class MapsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) { }
+        if (getArguments() != null) {
+        }
     }
 
     @Override
@@ -57,33 +66,69 @@ public class MapsFragment extends Fragment {
             e.printStackTrace();
         }
 
-        mMapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                googleMap = mMap;
-
-                // For showing a move to my location button
-//                googleMap.setMyLocationEnabled(true);
-
-                mMap.getUiSettings().setZoomControlsEnabled(true);
-
-                // For dropping a marker at a point on the Map
-                LatLng epitech = new LatLng(48.8151239, 2.3631254);
-                googleMap.addMarker(new MarkerOptions().position(epitech).title("Marker Title").snippet("Marker Description"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(epitech).zoom(14).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
-        });
+        mMapView.getMapAsync(this);
 
         return rootView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        checkLocationPermission();
+        mGoogleMap.setOnMyLocationButtonClickListener(this);
+
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        // For dropping a marker at a point on the Map
+//        LatLng epitech = new LatLng(48.8151239, 2.3631254);
+//        mGoogleMap.addMarker(new MarkerOptions().position(epitech).title("Marker Title").snippet("Marker Description"));
+
+        // For zooming automatically to the location of the marker
+//        CameraPosition cameraPosition = new CameraPosition.Builder().target(epitech).zoom(14).build();
+//        mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    private void checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_LOCATION);
+        } else {
+            mGoogleMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkLocationPermission();
+                } else {
+                    Toast.makeText(getActivity(), "Location permission denied.", Toast.LENGTH_LONG).show();
+                    mPermissionDenied = true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
     }
 
     @Override
     public void onResume() {
         super.onResume();
         mMapView.onResume();
+//        if (mPermissionDenied) {
+//            // Permission was not granted, display error dialog.
+//            Toast.makeText(getActivity(), "Location permission denied.", Toast.LENGTH_LONG).show();
+//            mPermissionDenied = false;
+//        }
     }
 
     @Override
