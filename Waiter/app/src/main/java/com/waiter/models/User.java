a6d5f511e4a6386680ecef207d0bd12f7721ebb1
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-05-24 06:50:36 -0700

    add user model

---
+++ Waiter/app/src/main/java/com/waiter/models/User.java
@@ -0,0 +1,146 @@
+package com.waiter.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("firstname")
+    @Expose
+    private String firstname;
+    @SerializedName("lastname")
+    @Expose
+    private String lastname;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("type")
+    @Expose
+    private Integer type;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("confirmToken")
+    @Expose
+    private String confirmToken;
+    @SerializedName("__v")
+    @Expose
+    private Integer v;
+    @SerializedName("waiterCurrentEvent")
+    @Expose
+    private Object waiterCurrentEvent;
+    @SerializedName("createdAt")
+    @Expose
+    private String createdAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getConfirmToken() {
+        return confirmToken;
+    }
+
+    public void setConfirmToken(String confirmToken) {
+        this.confirmToken = confirmToken;
+    }
+
+    public Integer getV() {
+        return v;
+    }
+
+    public void setV(Integer v) {
+        this.v = v;
+    }
+
+    public Object getWaiterCurrentEvent() {
+        return waiterCurrentEvent;
+    }
+
+    public void setWaiterCurrentEvent(Object waiterCurrentEvent) {
+        this.waiterCurrentEvent = waiterCurrentEvent;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", type=" + type +
+                ", status='" + status + '\'' +
+                ", confirmToken='" + confirmToken + '\'' +
+                ", v=" + v +
+                ", waiterCurrentEvent=" + waiterCurrentEvent +
+                ", createdAt='" + createdAt + '\'' +
+                '}';
+    }
+}
