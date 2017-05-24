208d900c39c8feb47fd443168488673707add7d9
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-05-24 06:53:59 -0700

    add check email available and signup route in Waiter client

--- Waiter/app/src/main/java/com/waiter/network/WaiterClient.java
+++ Waiter/app/src/main/java/com/waiter/network/WaiterClient.java
@@ -1,13 +1,24 @@
 package com.waiter.network;
 
 import com.waiter.models.RequestLogin;
+import com.waiter.models.RequestSignup;
 import com.waiter.models.ResponseLogin;
+import com.waiter.models.ResponseSignup;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface WaiterClient {
     @POST("/user/login")
     Call<ResponseLogin> login(@Body RequestLogin requestLogin);
+
+    @POST("/user")
+    Call<ResponseSignup> signup(@Body RequestSignup requestSignup);
+
+    @GET("/user/available/{email}")
+    Call<ResponseBody> checkEmailAvailable(@Path("email") String email);
 }
