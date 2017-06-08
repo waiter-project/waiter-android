eee415b11c5f56e9fce31f4beacb7d474a2e6000
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-06-08 00:15:39 -0700

    remove dummy

--- Waiter/app/src/main/java/com/waiter/EventRecyclerViewAdapter.java
+++ Waiter/app/src/main/java/com/waiter/EventRecyclerViewAdapter.java
@@ -9,16 +9,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.waiter.EventFragment.OnListFragmentInteractionListener;
-import com.waiter.dummy.DummyContent.DummyItem;
 import com.waiter.models.Event;
 
 import java.util.List;
 
-/**
- * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
- * specified {@link OnListFragmentInteractionListener}.
- * TODO: Replace the implementation with code for your data type.
- */
 public class EventRecyclerViewAdapter extends RecyclerView.Adapter<EventRecyclerViewAdapter.ViewHolder> {
 
     private static final String TAG = "EventRecyclerViewAdapte";
